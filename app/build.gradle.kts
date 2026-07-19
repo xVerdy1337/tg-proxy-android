0041405315d39a0a56d72c0ca8938cc4f694b0a9
theJorDea <klevin3701@gmail.com>
2026-07-19 18:45:54 +0300

    Set app version to 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.tgwsproxy"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         ndk {
             abiFilters.add("arm64-v8a")
