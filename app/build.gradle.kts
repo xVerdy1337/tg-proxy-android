9607b5ee05c55dbdfdfa735fa7f899fdf2a92548
xVerdy1337 <artgrach86@gmail.com>
2026-09-11 21:21:11 +0300

    chore(release): v1.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.tgwsproxy"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.3.1"
 
         ndk {
             abiFilters.add("arm64-v8a")
